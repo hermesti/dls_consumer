5827a3a4c14410fc0797b405fa97b199af543900
Alberto Santos Arenillas <alberto.santos@sstrategy.io>
2018-11-15 15:31:11 +0100

    Separated database for tests and main.

--- src/main/java/io/sstrategy/demo/dls/consumer/DlsConsumerApplication.java
+++ src/main/java/io/sstrategy/demo/dls/consumer/DlsConsumerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /*
@@ -29,16 +30,52 @@ public class DlsConsumerApplication {
 	@Value("${connection.url}")
 	private String connectionUrl;
 	
+	@Value("${connection.username}")
+	private String connectionUsername;
+	
+	@Value("${connection.password}")
+	private String connectionPassword;
+	
+	
+	@Value("${connection.test.driver}")
+	private String connectionTestDriver;
+	
+	@Value("${connection.test.url}")
+	private String connectionTestUrl;
+	
+	@Value("${connection.test.username}")
+	private String connectionTestUsername;
+	
+	@Value("${connection.test.password}")
+	private String connectionTestPassword;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DlsConsumerApplication.class, args);
 	}
 	
     @Bean
+    @Profile("!test")
     public DataSource dataSource() {
     	
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(connectionDriver);
         dataSource.setUrl(connectionUrl);
+        dataSource.setUsername(connectionUsername);
+        dataSource.setPassword(connectionPassword);
+        
+        return dataSource;
+    }
+    
+    @Bean
+    @Profile("test")
+    public DataSource dataSourceTest() {
+    	
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(connectionTestDriver);
+        dataSource.setUrl(connectionTestUrl);
+        dataSource.setUsername(connectionTestUsername);
+        dataSource.setPassword(connectionTestPassword);
         
         return dataSource;
     }
--- src/main/java/io/sstrategy/demo/dls/consumer/controller/DemoController.java
+++ src/main/java/io/sstrategy/demo/dls/consumer/controller/DemoController.java
@@ -8,21 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.sstrategy.demo.dls.library.DatabaseService;
 import io.sstrategy.demo.dls.library.Service;
+import io.sstrategy.demo.dls.library.model.Animal;
 
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
 	
 	@Autowired
-	private Service serviceClass;
-
+	private Service service;
+	
+	@Autowired
+	private DatabaseService databaseService;
+	
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = "text/html; charset=UTF-8")
 	public String get(
 			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
 		
-		return "<h1>Hello world!</h1>" 
-				+ "<p>Service class message: " + serviceClass.getMessage() + "</p>";
+		Animal cat = new Animal();
+		cat.setName("cat");
+		databaseService.storeAnimal(cat);
+		
+		return "<h1>Hello world!</h1>"
+				+ "<p>Service class message: " + service.getMessage() + "</p>"
+				+ "<p>And I have stored a cat!</p>";
 	}
 	
 }
